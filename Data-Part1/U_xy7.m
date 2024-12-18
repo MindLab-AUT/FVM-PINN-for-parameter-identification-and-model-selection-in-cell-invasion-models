0619bfcc3bb001acee690135ffb9281a09f182f8
MindLab-AUT <mindlab.aut@gmail.com>
2024-12-18 19:24:32 +0330

    Add files via upload

---
+++ Data-Part1/U_xy7.m
@@ -0,0 +1,20 @@
+% Number of Data points in each time step
+N_point = 22500;
+
+% Number of time steps
+N_t = 77;
+
+% Define U_total
+U_total = zeros(N_point * N_t, 1);
+
+for i = 1:N_t
+    U = density(:, :, i);
+    U(isnan(U)) = 0;  % Replace NaN values with 0
+    U = reshape(U, N_point, 1);
+    U_total((i - 1) * N_point + 1:i * N_point) = U;
+end
+
+% Save matrix
+U_7 = [U_total];
+save('U_total_7.mat', 'U_7');
+
